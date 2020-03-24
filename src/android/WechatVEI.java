992e6d2451634f34f43804846f9ef2b7bbd353c5
Abigail MARIANO <abigail.mariano@value-exch.com>
2020-03-24 11:00:31 +0800

    Updated plugin. Added initialize method.

--- src/android/WechatVEI.java
+++ src/android/WechatVEI.java
@@ -1,5 +1,51 @@
 package com.vei.wechat;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+import android.os.Build;
+import android.util.Base64;
+import android.util.Log;
+import android.webkit.URLUtil;
+
+import com.tencent.mm.opensdk.modelbiz.WXLaunchMiniProgram;
+import com.tencent.mm.opensdk.modelmsg.SendAuth;
+import com.tencent.mm.opensdk.modelmsg.SendMessageToWX;
+import com.tencent.mm.opensdk.modelmsg.WXAppExtendObject;
+import com.tencent.mm.opensdk.modelmsg.WXEmojiObject;
+import com.tencent.mm.opensdk.modelmsg.WXFileObject;
+import com.tencent.mm.opensdk.modelmsg.WXImageObject;
+import com.tencent.mm.opensdk.modelmsg.WXMediaMessage;
+import com.tencent.mm.opensdk.modelmsg.WXMusicObject;
+import com.tencent.mm.opensdk.modelmsg.WXTextObject;
+import com.tencent.mm.opensdk.modelmsg.WXVideoObject;
+import com.tencent.mm.opensdk.modelmsg.WXWebpageObject;
+import com.tencent.mm.opensdk.modelmsg.WXMiniProgramObject;
+import com.tencent.mm.opensdk.modelpay.PayReq;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+
+import com.tencent.mm.opensdk.modelbiz.ChooseCardFromWXCardPackage;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaArgs;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayInputStream;
+import org.apache.cordova.CordovaPreferences;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
@@ -12,12 +58,34 @@ import org.json.JSONObject;
  */
 public class WechatVEI extends CordovaPlugin {
 
+    public static final String TAG = "Cordova.Plugin.Wechat";
+
+    public static final String PREFS_NAME = "Cordova.Plugin.Wechat";
+    public static final String WXAPPID_PROPERTY_KEY = "wechatappid";
+
+    public static final String ERROR_WECHAT_NOT_INSTALLED = "未安装微信";
+    public static final String ERROR_INVALID_PARAMETERS = "参数格式错误";
+    public static final String ERROR_SEND_REQUEST_FAILED = "发送请求失败";
+    public static final String ERROR_WECHAT_RESPONSE_COMMON = "普通错误";
+    public static final String ERROR_WECHAT_RESPONSE_USER_CANCEL = "用户点击取消并返回";
+    public static final String ERROR_WECHAT_RESPONSE_SENT_FAILED = "发送失败";
+    public static final String ERROR_WECHAT_RESPONSE_AUTH_DENIED = "授权失败";
+    public static final String ERROR_WECHAT_RESPONSE_UNSUPPORT = "微信不支持";
+    public static final String ERROR_WECHAT_RESPONSE_UNKNOWN = "未知错误";
+
+    protected static CallbackContext currentCallbackContext;
+    protected static IWXAPI wxAPI;
+    protected static String appId;
+    protected static CordovaPreferences wx_preferences;
+
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("wechatPay")) {
             String message = args.getString(0);
             this.wechatPay(message, callbackContext);
             return true;
+        } else if (action.equals("sendPaymentRequest")) {
+            return this.sendPaymentRequest(args, callbackContext);
         }
         return false;
     }
@@ -28,5 +96,94 @@ public class WechatVEI extends CordovaPlugin {
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
+    }
+
+    protected boolean sendPaymentRequest(CordovaArgs args, CallbackContext callbackContext) {
+
+        // check if # of arguments is correct
+        final JSONObject params;
+        try {
+            params = args.getJSONObject(0);
+        } catch (JSONException e) {
+            callbackContext.error(ERROR_INVALID_PARAMETERS);
+            return true;
+        }
+
+        PayReq req = new PayReq();
+
+        try {
+            // final String appid = params.getString("appid");
+            // final String savedAppid = getSavedAppId(cordova.getActivity());
+            // if (!savedAppid.equals(appid)) {
+            //     this.saveAppId(cordova.getActivity(), appid);
+            // }
+            req.appId = getAppId(preferences);
+            req.partnerId = params.has("mch_id") ? params.getString("mch_id") : params.getString("partnerid");
+            req.prepayId = params.has("prepay_id") ? params.getString("prepay_id") : params.getString("prepayid");
+            req.nonceStr = params.has("nonce") ? params.getString("nonce") : params.getString("noncestr");
+            req.timeStamp = params.getString("timestamp");
+            req.sign = params.getString("sign");
+            req.packageValue = "Sign=WXPay";
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+
+            callbackContext.error(ERROR_INVALID_PARAMETERS);
+            return true;
+        }
+
+        final IWXAPI api = getWxAPI(cordova.getActivity());
+
+        if (api.sendReq(req)) {
+            Log.i(TAG, "Payment request has been sent successfully.");
+
+            // send no result
+            sendNoResultPluginResult(callbackContext);
+        } else {
+            Log.i(TAG, "Payment request has been sent unsuccessfully.");
+
+            // send error
+            callbackContext.error(ERROR_SEND_REQUEST_FAILED);
+        }
+
+        return true;
+    }
+
+    public static String getAppId(CordovaPreferences f_preferences) {
+        if (appId == null) {
+            if(f_preferences != null) {
+                appId = f_preferences.getString(WXAPPID_PROPERTY_KEY, "");
+            }else if(wx_preferences != null){
+                appId = wx_preferences.getString(WXAPPID_PROPERTY_KEY, "");
+            }
+        }
+
+        return appId;
+    }
+
+    public static IWXAPI getWxAPI(Context ctx) {
+        if (wxAPI == null) {
+            String appId = getSavedAppId(ctx);
+
+            if (!appId.isEmpty()) {
+                wxAPI = WXAPIFactory.createWXAPI(ctx, appId, true);
+            }
+        }
+
+        return wxAPI;
+    }
+
+    private void sendNoResultPluginResult(CallbackContext callbackContext) {
+        // save current callback context
+        currentCallbackContext = callbackContext;
+
+        // send no result and keep callback
+        PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
+        result.setKeepCallback(true);
+        callbackContext.sendPluginResult(result);
+    }
+
+    public static String getSavedAppId(Context ctx) {
+        SharedPreferences settings = ctx.getSharedPreferences(PREFS_NAME, 0);
+        return settings.getString(WXAPPID_PROPERTY_KEY, "");
     }
 }
--- src/ios/WechatVEI.m
+++ src/ios/WechatVEI.m
@@ -17,6 +17,25 @@
 
 @implementation WechatVEI
 
+- (void)pluginInitialize {
+    NSString* appId = [[self.commandDelegate settings] objectForKey:@"wechatappid"];
+    
+    NSString* universalLink = @"https://ecride.hksr.org.hk/RiderWeb/";
+    
+    if (appId && ![appId isEqualToString:self.wechatAppId]) {
+        self.wechatAppId = appId;
+        [WXApi registerApp: appId universalLink: universalLink];
+        
+        bool appRegistrationSuccess = [WXApi registerApp: appId universalLink: universalLink];
+        
+        if (appRegistrationSuccess) {
+            NSLog(@"cordova-plugin-vei-wechat-pay has been initialized. Wechat SDK Version: %@. APP_ID: %@.", [WXApi getApiVersion], appId);
+        } else {
+            NSLog(@"Failed to register App.");
+        }
+    }
+}
+
 - (void)wechatPay:(CDVInvokedUrlCommand *)command
 {
     CDVPluginResult* pluginResult = nil;
@@ -63,12 +82,6 @@
 
     PayReq *req = [[PayReq alloc] init];
 
-    // NSString *appId = [params objectForKey:requiredParams[5]];
-    // if (appId && ![appId isEqualToString:self.wechatAppId]) {
-    //     self.wechatAppId = appId;
-    //     [WXApi registerApp: appId];
-    // }
-
     req.partnerId = [params objectForKey:requiredParams[0]];
     req.prepayId = [params objectForKey:requiredParams[1]];
     req.timeStamp = [[params objectForKey:requiredParams[2]] intValue];
@@ -76,18 +89,12 @@
     req.package = @"Sign=WXPay";
     req.sign = [params objectForKey:requiredParams[4]];
 
-    // if ([WXApi sendReq:req])
-    // {
-    //     // save the callback id
-    //     self.currentCallbackId = command.callbackId;
-    // }
-    // else
-    // {
-    //     [self failWithCallbackID:command.callbackId withMessage:@"Failed to send request!"];
-    // }
-
     [WXApi sendReq:req completion:^(BOOL success) {
-        self.currentCallbackId = command.callbackId;
+        if (success) {
+            self.currentCallbackId = command.callbackId;
+        } else {
+            [self failWithCallbackID:command.callbackId withMessage:@"Send request failed"];
+        }
     }];
 }
 
